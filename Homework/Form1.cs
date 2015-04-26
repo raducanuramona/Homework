08ffa3d437075c48a0fed71cf910a711f8de71ad
RaducanuRamona <raducanu.ramona5@gmail.com>
2015-04-26 16:56:58 +0300

    TextBox and List

---
+++ Homework/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework
+{
+    public partial class Form1 : Form
+    {
+        public List<string> lang = new List<string> { "en", "fr", "de" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
